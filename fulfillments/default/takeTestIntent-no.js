24c928f279234c9afd85821aecf0609262bcf376
michael <me@michaelseeber.com>
2020-09-19 21:36:41 +0200

    test image yes

--- fulfillments/default/takeTestIntent-no.js
+++ fulfillments/default/takeTestIntent-no.js
@@ -13,6 +13,8 @@ module.exports = {
                 "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/e/e9/Felis_silvestris_silvestris_small_gradual_decrease_of_quality.png"
             }
 
+            
+
 
             }
         , {sendAsMessage: true, rawPayload: true}));
--- fulfillments/default/takeTestIntent-yes.js
+++ fulfillments/default/takeTestIntent-yes.js
@@ -9,26 +9,15 @@ module.exports = {
         agent.add("Have a Nice Day!");
 
         
-        agent.add(new Payload(agent.UNSPECIFIED, {
-            "richContent": [
-                [
-                    {
-                        "type": "image",
-                        "rawUrl": "https://github.com/akshitgupta95/hackzurich2020/blob/master/images/JoBrownBear-AgADiwUAAj-VzAo.gif",
-                    },
-                    {
-                        "type": "info",
-                        "title": "Great!",
-                        "subtitle": "Keep up with the tests!",
-                    }
-                ]
-                ]
-
+        agent.add(new Payload(agent.TELEGRAM,{
+            "image": {
+                "imageUri": "https://github.com/akshitgupta95/hackzurich2020/blob/master/images/JoBrownBear-AgADiwUAAj-VzAo.gif"
+              }
 
         }, {sendAsMessage: true, rawPayload: true}));
         agent.end("This task is now complete. Click the continue button in left of your screen to proceed");
 
-        
+    
 
     }
 
