4fef23e847ecdbe8e416b8e04479d934233ce71b
michael <me@michaelseeber.com>
2020-09-19 20:46:31 +0200

    test images

--- fulfillments/default/takeTestIntent-yes.js
+++ fulfillments/default/takeTestIntent-yes.js
@@ -5,8 +5,30 @@ module.exports = {
     fulfillment: function (agent) {
 
         agent.add('Thanks for taking a test');
-        agent.add('show overview image here');
+        
         agent.add("Have a Nice Day!");
+
+        
+        agent.add(new Payload(agent.UNSPECIFIED, {
+            "richContent": [
+                [
+                    {
+                        "type": "image",
+                        "rawUrl": "https://github.com/akshitgupta95/hackzurich2020/blob/master/images/JoBrownBear-AgADiwUAAj-VzAo.gif",
+                    },
+                    {
+                        "type": "info",
+                        "title": "Great!",
+                        "subtitle": "Keep up with the tests!",
+                    }
+                ]
+                ]
+
+
+        }, {sendAsMessage: true, rawPayload: true}));
+        agent.end("This task is now complete. Click the continue button in left of your screen to proceed");
+
+        
 
     }
 
